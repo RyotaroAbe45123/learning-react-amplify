2d57490c7338ed561a9dbd15e1629715e35c6b74
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-01-22 17:01:27 +0900

    release v3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
-        <img src="https://images.unsplash.com/photo-1538370965046-79c0d6907d47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3269&q=80" width="640" height="480" />
+        <h2>kawada shinkura v3</h2>
       </header>
     </div>
   );
