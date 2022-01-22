4bda8da3539fde3fe982b24e496b05edc05d4440
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-01-22 16:45:02 +0900

    release V3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
+        <img src="https://images.unsplash.com/photo-1538370965046-79c0d6907d47?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80">
       </header>
     </div>
   );
