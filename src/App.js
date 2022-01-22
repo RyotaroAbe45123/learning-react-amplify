88519790869325b4874e03baec248909d431f1e7
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2022-01-22 16:39:09 +0900

    release V2

--- src/App.js
+++ src/App.js
@@ -7,17 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.編集したら保存する。
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
